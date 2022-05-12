7c2b10fa2b7855c660a1c518e569a4246c92e1ab
Jung Su <wjdwjdtn92@naver.com>
2022-05-12 23:34:47 +0900

    basename 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import Coins from "./routes/Coins";
 import Price from "./routes/Price";
 
 function Router() {
-    return (<BrowserRouter>
+    return (<BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/:coinId" element={<Coin />}>
                 <Route path="price" element={<Price />} />
